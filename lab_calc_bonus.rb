20b0762bd22953851dcc44f94cbf592ff4cbdf33
Guillaume Perrin <gperrin01@gmail.com>
2015-05-18 19:03:15 +0100

    all finished

--- lab_calc_bonus.rb
+++ lab_calc_bonus.rb
@@ -1,21 +1,4 @@
 ### FUNCTIONS
-def calcs(num1, num2, math_op)
-  case math_op
-  when '+'
-    puts num1 + num2
-  when '-'
-    puts num1 - num2
-  when 'x'
-    puts num1 * num2
-  when '/'
-    puts num1 / num2
-  when 'p'
-    puts num1**num2
-  when 's'
-    puts Math.sqrt(num1)*num2
-  end
-end
-
 def correct_operator(math_op, op_list=[])
   # user re-inputs operator until it is one of the authorized values
   math_op = gets.chomp
@@ -26,6 +9,88 @@ def correct_operator(math_op, op_list=[])
   math_op
 end
 
+def calc_basics
+  print 'Type first number: '
+  num1 = gets.to_f
+  print 'Choose operation to perform (+,-,x,/): '
+  math_op = correct_operator(math_op, ['+', '-', 'x', '/'])
+  print 'Type second number: '
+  num2 = gets.to_f
+
+  case math_op
+  when '+'
+    puts num1 + num2
+  when '-'
+    puts num1 - num2
+  when 'x'
+    puts num1 * num2
+  when '/'
+    puts num1 / num2
+  end
+end  
+# end calc_basics
+
+def calc_advanced
+  print 'Type first number: '
+  num1 = gets.to_f
+  print '(p)ower or (s)quare?: '
+  math_op = correct_operator(math_op, ['p', 's'])
+
+  if math_op == 'p'
+    print 'Type second number: '
+    num2 = gets.to_f
+    puts num1**num2
+  elsif math_op == 's'
+    puts Math.sqrt(num1)
+  end
+end  
+# end calc_advanced
+
+def calc_mortgage
+  print 'Loan in £: '
+  loan = gets.to_f
+  print 'APR in %: '
+  apr = gets.to_f / 100 / 12
+  print 'Term in months: '
+  term = gets.to_f
+  
+  temp = (1+apr)**term
+  payment = (loan * apr * temp / (temp - 1)).round(2)
+  puts "Monthly payment: £#{payment}"
+end
+
+def calc_bmi
+  print 'Mass in kg: '
+  mass = gets.to_f
+  print 'Height in meters: '
+  height = gets.to_f
+
+  bmi = (mass / (height**2) ).round(2)
+  puts "BMI: #{bmi}"
+end
+
+def calc_trip
+  print 'Distance in miles: '
+  dist = gets.to_f
+  print 'MPG: '
+  mpg = gets.to_f
+  print 'Cost in £/gallon: '
+  cost = gets.to_f
+  print 'Speed in mph: '
+  speed = gets.to_f
+
+  time = (dist / speed).round(2)
+
+  if mpg > 60 
+    actualMPG = [0, mpg - 2*(speed-60)].max
+  else actualMPG = mpg
+  end
+
+  cost = (dist / actualMPG * cost).round(2)
+  puts "Your trip will take #{time} hours and cost $#{cost}"
+end
+
+
 ##### 
 #THE MAIN FUNCTION
 ##########
@@ -33,35 +98,23 @@ end
 def calc_it
   system "clear"
   puts '*** Calc It ***'
-  print 'Select (b)asic or (a)dvanced calculator, or (q)uit: '
+  print 'Select (b)asic, (a)dvanced, (m)ortgage, (bmi), (t)rip, or (q)uit: '
   calc_type = gets.chomp.downcase
 
+  # while calc_type != 'q'
   return  if calc_type =='q'
- 
-  print 'Type first number: '
-  num1 = gets.to_f
 
-  if calc_type == 'b'
-
-    print 'Choose operation to perform (+,-,x,/): '
-    math_op = correct_operator(math_op, ['+', '-', 'x', '/'])
-
-    print 'Type second number: '
-    num2 = gets.to_f
-    puts calcs(num1, num2, math_op)
-
-  elsif calc_type == 'a'
-
-    print '(p)ower or (s)quare?: '
-    math_op = correct_operator(math_op, ['p', 's'])
-
-    if math_op == 'p'
-      print 'Type second number: '
-      num2 = gets.to_f
-      puts calcs(num1, num2, math_op)
-    elsif math_op == 's'
-      puts calcs(num1, 1, math_op)
-    end
+  case calc_type
+  when 'b'
+    calc_basics
+  when 'a'
+    calc_advanced
+  when 'm'
+    calc_mortgage
+  when 'bmi'
+    calc_bmi
+  when 't'
+    calc_trip
   end
 
   print 'press any key to start again, or (q) to quit: '
@@ -73,7 +126,6 @@ def calc_it
   end
 
 end
-
 
 calc_it
 
